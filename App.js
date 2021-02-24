ebb70fa3d6a1283086ead3998c519988f9316d30
Rahim <belkhirrahim@gmail.com>
2021-02-24 16:18:28 +0100

    UpdateRahim

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Bonjour je vais faire un commit numero 56</Text>
+      <Text>Modif de Rahim</Text>
       <StatusBar style="auto" />
     </View>
   );
